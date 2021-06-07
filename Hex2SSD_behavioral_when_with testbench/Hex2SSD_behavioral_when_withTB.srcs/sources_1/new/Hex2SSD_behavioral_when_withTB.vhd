60d38733044619d0758be2698c59146b742a88d3
KKMinu <58761098+MinuLoL@users.noreply.github.com>
2021-06-07 23:33:32 +0900

    Update Hex2SSD_behavioral_when_withTB.vhd

--- Hex2SSD_behavioral_when_with testbench/Hex2SSD_behavioral_when_withTB.srcs/sources_1/new/Hex2SSD_behavioral_when_withTB.vhd
+++ Hex2SSD_behavioral_when_with testbench/Hex2SSD_behavioral_when_withTB.srcs/sources_1/new/Hex2SSD_behavioral_when_withTB.vhd
@@ -4,7 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity HEX2SSD2 is
     port(
         x : in STD_LOGIC_VECTOR(3 downto 0);
-        f : out STD_LOGIC_VECTOR(7 downto 0);
+        f : out STD_LOGIC_VECTOR(6 downto 0);
         an: out STD_LOGIC_VECTOR(3 downto 0) );
 end HEX2SSD2;
 
